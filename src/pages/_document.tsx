333d3f871cdba47630f730f517506f92dd4bd42c
Diego Torres Mijarra <134062570+DiegoTorresMijarra@users.noreply.github.com>
2025-04-19 11:15:34 +0200

    add .gitignore

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="es">
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
